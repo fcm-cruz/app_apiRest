34df3613f81412481909d994e28f5fbbb7b31308
fcm-cruz <fernando.cruzm@icloud.com>
2024-10-14 12:11:50 -0300

    Hotfix: sistema básico para cadastro de nome e idade

---
+++ app_funcoes/public/cadastro.js
@@ -0,0 +1,51 @@
+// Cadastro de funcionários
+document.querySelector('#cadastrar').addEventListener('click', (e) => {
+    e.preventDefault();
+
+    // Cadastrar pessoa
+
+    function pessoas(){
+
+        let inputs = document.querySelectorAll('#formCadastro input[type="text"], input[type="number"]');
+
+        let array = Array.from(inputs).map(input => input.value);
+
+        let boxMsg = document.querySelector('#box');
+
+        let arr = [];
+
+        array.forEach((element) => {
+            
+            if(element == ""){
+            
+            boxMsg.innerHTML = `<div class="warning"><p>Preencha todos os campos!</p></div>`;   
+            
+            console.error(`Erro: Formulário de cadastro não preenchido.`);
+            
+            }
+            else{
+
+                arr.push(element);
+
+                boxMsg.innerHTML = `<div class="confirm">
+                        <p>Cadastro realizado com sucesso!</p>
+                        <p>Nome: ${arr[0]}</p><p>Idade: ${arr[1]}</p>
+                    </div>`;
+        
+                console.log(`Status ok: Formulário de cadastro preenchido.`);
+
+                setInterval(() => {
+
+                    boxMsg.classList.remove('confirm').
+                    boxMsg.classList.add('remove')
+
+                }, 2000);
+            }
+        }      
+        
+        );
+    }
+    
+    pessoas();
+
+})
